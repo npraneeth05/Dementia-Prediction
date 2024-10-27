af3745f4a41098ae3ecf77bd16f2bb0c326f6b65
Praneeth Nellipudi <119812510+npraneeth05@users.noreply.github.com>
2024-10-27 13:05:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,9 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/npraneeth05/Dementia-Prediction/refs/heads/master/dementia_dataset.csv')
   df
-
-  st.write('**X**')
-  X = df.drop('Group')
-  X
- 
-  st.write('**Y**')
-  Y = df.Group
-  Y
   
 with st.sidebar:
-  #Group,Visit,MR Delay,M/F,Hand,Age,EDUC,SES,MMSE,CDR,eTIV,nWBV,ASF
   st.header('Input Features')
+  #Age,EDUC,MMSE,CDR,eTIV,nWBV
+  Gender = st.selectbox('Gender', ('Male', 'Female'))
   
