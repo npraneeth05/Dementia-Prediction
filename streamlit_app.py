b51e9f9f1ecb7ec2ef82af1b72231220473f6aee
Praneeth Nellipudi <119812510+npraneeth05@users.noreply.github.com>
2024-10-27 12:16:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('Dementia Prediction App 🤖')
 
 st.info('Early diagnosis for better future..🩺🔬')
 
-df = pd.read_csv('https://raw.githubusercontent.com/npraneeth05/Dementia-Prediction/refs/heads/master/dementia_dataset.csv')
-df
+with st.expander('Data'):
+  st.write('Raw Data')
+ df = pd.read_csv('https://raw.githubusercontent.com/npraneeth05/Dementia-Prediction/refs/heads/master/dementia_dataset.csv')
+ df
