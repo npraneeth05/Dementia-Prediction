9c7373cee14dea48d34f47f1109dd31e800b3415
Praneeth Nellipudi <119812510+npraneeth05@users.noreply.github.com>
2024-10-27 12:34:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,16 @@ st.title('Dementia Prediction App 🤖')
 st.info('Early diagnosis for better future..🩺🔬')
 
 with st.expander('Data'):
-  st.write('Raw Data')
+  st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/npraneeth05/Dementia-Prediction/refs/heads/master/dementia_dataset.csv')
   df
+
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+  st.write('**Y**')
+  Y = df.species
+  Y
+with st.expander('Data Visualization'):
+  st.scatter_chart(data=df, x='Group', y='Age', color='species'
